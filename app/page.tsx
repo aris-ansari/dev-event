7bb4e738e591072d3eb3941f84062f66e28f038c
Aris Ansari <arisansari.work@gmail.com>
2026-06-04 15:49:37 +0530

    finalize deployment

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,14 @@ import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database/event.model";
 import { cacheLife } from "next/cache";
-// import events from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-console.log("BASE_URL ==========", BASE_URL);
 
 const Page = async () => {
   "use cache";
   cacheLife("hours");
 
   const response = await fetch(`${BASE_URL}/api/events`);
-
-  console.log("Content-Type ==========", response.headers.get("content-type"));
-
   const { events } = await response.json();
 
   return (
